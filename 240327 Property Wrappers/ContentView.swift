a1c3a8c34e66200a6f23c4dea7e5bf30c18648a1
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-03-27 15:25:28 +0100

    Initial Commit

---
+++ 240327 Property Wrappers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  240327 Property Wrappers
+//
+//  Created by Luis on 27/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 240327 Property Wrappers/_40327_Property_WrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40327_Property_WrappersApp.swift
+//  240327 Property Wrappers
+//
+//  Created by Luis on 27/3/24.
+//
+
+import SwiftUI
+
+@main
+struct _40327_Property_WrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
